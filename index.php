f02563e0ff546e5bc9cee570c42394c06eaa5902
zaydalucia25 <zaydalcc25@gmail.com>
2020-09-14 15:25:39 -0600

    2

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
-<?php 
+<?php
   include 'estilos.php';
-?> 
+?>
 
 <html>
   <head>
     <title>PHP Test</title>
   </head>
   <body>
-    <?php echo '<p class="font-weight-light">Hola amigos de youtube hoy os traigo un nuevo video tutorial sobre como crear un proyecto en php, lo primero que tenemos que hacer es crearse una cuenta en github</p>'; ?> 
+    <?php echo '<p class="font-weight-light">Hola amigos de youtube hoy os traigo un nuevo video tutorial sobre como crear un proyecto en php, lo primero que tenemos que hacer es crearse una cuenta en github</p>'; ?>
+    <h1> HOLA </h1>
   </body>
 </html>
-
