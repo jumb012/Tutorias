995fad1f0a6112acf1e175ad2ce0c7084a41e468
Jassiel Martinez Beltran <ju.martinez16@info.uas.edu.mx>
2020-09-14 16:35:13 -0600

    1.1

--- Equipo3/alta_carr.php
+++ Equipo3/alta_carr.php
@@ -1,12 +1,43 @@
-<?php 
+<?php
   include '../estilos.php';
-?> 
+?>
 
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Alta de Carrera</title>
+	ya lo puse en estilos
+
+
+
 </head>
 <body>
-  <h1 class="h1"> Hola </h1>
+<form>
+	<form>
+	  <div class="form-row col-md-12">
+	    <div class="col-md-6 mb-3">
+	      <label for="nombreCarrera"><strong>Nombre</strong></label>
+	      <input type="text" class="form-control" id="nombreCarrera" required>
+	  	</div>
+	  </div>
+  <div class="form-row col-md-6">
+    <div class="col-md-6 mb-3">
+      <label for="matriculaCarrera"><strong>Matricula</strong></label>
+      <input type="text" class="form-control" id="matriculaCarrera" required>
+    </div>
+    <div class="col-md-6 mb-3">
+      <label for="abreviaturaCarrera"><strong>Abreviatura</strong></label>
+      <input type="text" class="form-control" id="abreviaturaCarrera" required>
+    </div>
+  </div>
+  <form>
+  <div class="form-group col-md-6">
+    <label for="subirArchivo"><strong>Lista de alumnos (.pdf)</strong></label>
+    <input type="file" class="form-control-file" id="subirArchivo">
+  </div>
+</form>
+		<button class="btn btn-primary" type="submit">Registrar Carrera</button>
+</form>
+</form>
 </body>
+</html>
