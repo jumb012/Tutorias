56cc256fef748fe1027ee25354ee288bef7f3d19
Jassiel Martinez Beltran <ju.martinez16@info.uas.edu.mx>
2020-09-16 12:08:12 -0600

    1.1

--- index.php
+++ index.php
@@ -7,7 +7,9 @@
     <title>PHP Test</title>
   </head>
   <body>
-    <?php echo '<p class="font-weight-light">Hola amigos de youtube hoy os traigo un nuevo video tutorial sobre como crear un proyecto en php, lo primero que tenemos que hacer es crearse una cuenta en github</p>'; ?>
-    <h1> HOLA aqui estuvo la zayda </h1>
+    <?php
+    echo '<p class="font-weight-light">Hola amigos de youtube hoy os traigo un nuevo video tutorial sobre como crear un proyecto en php, lo primero que tenemos que hacer es crearse una cuenta en github</p>';
+    ?>
+    <p class="display-1">HOLA aqui estuvo la zayda</p>
   </body>
 </html>
